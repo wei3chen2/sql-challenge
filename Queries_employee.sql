8c48533979ee8d392b2c11d0f866a8536d76f331
wei3chen2 <120448264+wei3chen2@users.noreply.github.com>
2023-05-05 23:03:29 -0700

    Employee SQL

--- Queries_employee.sql
+++ Queries_employee.sql
@@ -4,7 +4,7 @@
 
 -- Modify this code to update the DB schema diagram.
 
-CREATE TABLE "Employees" (
+CREATE TABLE ""Employees"" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR(20)   NOT NULL,
     "birth_date" DATE   NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE "Employees" (
     "last_name" VARCHAR(255)   NOT NULL,
     "sex" VARCHAR(5)   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
+    CONSTRAINT "pk_"Employees"" PRIMARY KEY (
         "emp_no"
      )
 );
@@ -57,7 +57,7 @@ CREATE TABLE "Salaries" (
      )
 );
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+ALTER TABLE ""Employees"" ADD CONSTRAINT "fk_"Employees"_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "Titles" ("title_id");
 
 ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
@@ -76,55 +76,56 @@ ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
 
 -- 1. List the employee number, last name, first name, sex, and salary of each employee
-SELECT employees.emp_no AS "Employee_Number", employees.last_name, employees.first_name, employees.sex, salaries.salary
-FROM employees
-JOIN salaries ON employees.emp_no = salaries.emp_no;
+SELECT "Employees".emp_no AS "Employee_Number", "Employees".last_name,  
+       "Employees".first_name, "Employees".sex, "Salaries".salary
+FROM "Employees"
+JOIN "Salaries" ON "Employees".emp_no = "Salaries".emp_no;
 
--- 2. List the first name, last name, and hire date for the employees who were hired in 1986 
+-- 2. List the first name, last name, and hire date for the "Employees" who were hired in 1986 
 SELECT first_name, last_name, hire_date
-FROM employees
-WHERE hire_date LIKE '%1986';
+FROM "Employees"
+WHERE hire_date > '1986-1-1'::date AND hire_date < '1987-1-1'::date;
 
 -- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name
-SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
-FROM dept_manager
-JOIN departments 
-ON  departments.dept_no = dept_manager.dept_no 
-JOIN employees 
-ON  employees.emp_no = dept_manager.emp_no;
+SELECT "Dept_manager".dept_no, "Departments".dept_name, "Dept_manager".emp_no, "Employees".last_name, "Employees".first_name
+FROM "Dept_manager"
+JOIN "Departments" 
+ON  "Departments".dept_no = "Dept_manager".dept_no 
+JOIN "Employees" 
+ON  "Employees".emp_no = "Dept_manager".emp_no;
 
 -- 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name
-SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
-FROM employees
-JOIN dept_emp 
-ON employees.emp_no = dept_emp.emp_no
-JOIN departments 
-ON dept_emp.dept_no = departments.dept_no;
+SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Departments".dept_name
+FROM "Employees"
+JOIN "Dept_emp"
+ON "Employees".emp_no = "Dept_emp".emp_no
+JOIN "Departments" 
+ON "Dept_emp".dept_no = "Departments".dept_no;
 
 -- 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
 SELECT first_name, last_name, sex
-FROM employees
+FROM "Employees"
 WHERE first_name = 'Hercules' and last_name LIKE 'B%';
 
 -- 6. List each employee in the Sales department , including their employee number, last name, and first name
-SELECT * FROM departments;
-SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
-FROM employees
-JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
-WHERE departments.dept_name = 'Sales';
+SELECT * FROM "Departments";
+SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Departments".dept_name
+FROM "Employees"
+JOIN "Dept_emp" ON "Employees".emp_no = "Dept_emp".emp_no
+JOIN "Departments" ON "Dept_emp".dept_no = "Departments".dept_no
+WHERE "Departments".dept_name = 'Sales';
 
 -- 7. List each employee in the Sales(d007) and Development (d005) departments, including their employee number, last name, first name, and department name.
-SELECT * FROM departments;
-SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
-FROM employees
-JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
-WHERE departments.dept_no = 'd005' or departments.dept_no = 'd007';
+SELECT * FROM "Departments";
+SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Departments".dept_name
+FROM "Employees"
+JOIN "Dept_emp" ON "Employees".emp_no = "Dept_emp".emp_no
+JOIN "Departments" ON "Dept_emp".dept_no = "Departments".dept_no
+WHERE "Departments".dept_no = 'd005' or "Departments".dept_no = 'd007';
 
 
--- 8. In descending order, list the frequency counts of all the employee last names, (i.e., how many employees share each last name)
+-- 8. In descending order, list the frequency counts of all the employee last names, (i.e., how many "Employees" share each last name)
 SELECT last_name, COUNT(last_name) AS "Frequency_Counts"
-FROM employees
+FROM "Employees"
 GROUP BY last_name
 ORDER BY "Frequency_Counts" DESC;
